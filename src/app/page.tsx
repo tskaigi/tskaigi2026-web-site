93fbb9abf6d7444a01e87459fbecb6f1b97a9f69
Kenta TSUNEMI <25561744+tocomi@users.noreply.github.com>
2025-11-15 16:13:12 +0900

    feat: tailwind のカラーテーマを設定 (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-bold text-track-toggle">
+      <h1 className="text-4xl font-bold text-blue-purple-500">
         TSKaigi 2026 ティザーサイト
       </h1>
     </main>
