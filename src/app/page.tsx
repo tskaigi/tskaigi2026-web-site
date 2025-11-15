3b023f4506de1ae1c5a2116942ffa4e72116a8cd
tocomi <tocomi0112@gmail.com>
2025-11-15 16:12:26 +0900

    feat: color theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-bold">TSKaigi 2026 ティザーサイト</h1>
+      <h1 className="text-4xl font-bold text-blue-purple-500">
+        TSKaigi 2026 ティザーサイト
+      </h1>
     </main>
   );
 }
