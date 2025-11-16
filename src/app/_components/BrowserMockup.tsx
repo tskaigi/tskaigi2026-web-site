c93fd116119102eeb020a013e6da74354d09ad9e
tocomi <tocomi0112@gmail.com>
2025-11-16 20:41:36 +0900

    feat: browser design

--- src/app/_components/BrowserMockup.tsx
+++ src/app/_components/BrowserMockup.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export function BrowserMockup({ children, className }: Props) {
   return (
-    <Card className={`overflow-hidden ${className}`}>
+    <Card className={`overflow-hidden py-0 rounded-lg ${className}`}>
       <div className="bg-zinc-100 px-4 py-2 border-b flex items-center space-x-2">
         <div className="flex space-x-1.5">
           <div className="w-3 h-3 rounded-full bg-red-500" />
