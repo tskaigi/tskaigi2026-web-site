302e73ad7ba591bac4b0a65d40894b1bd10c9d88
Ryo Ochiai <ryo.ochiai@kemuridama.jp>
2025-12-21 13:13:14 +0900

    :art: Add redirects for sponsors

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,20 @@ const nextConfig: NextConfig = {
   redirects: async () => {
     return [
       {
+        source: "/sponsors/call-for-sponsors",
+        destination: "https://docs.google.com/document/d/e/2PACX-1vRR4qf3tjPB5ZlrGjGln-FyX-TU1EXemj2XqFqyoedJlMFD2r8iYdcwGP5CNqvlvT4vcwYjNGzMjpTJ/pub",
+        permanent: false,
+        basePath: false,
+      },
+      {
+        source: "/sponsors/apply",
+        destination: "https://docs.google.com/forms/d/e/1FAIpQLSeTKamEr_jBcj_Q5ojp0f6XlFdICgph55xopdc_I53eHXcQCw/viewform",
+        permanent: false,
+        basePath: false,
+      },
+      {
         source: "/sponsors/inquiry",
-        destination: "https://example.com/test-form",
+        destination: "https://docs.google.com/forms/d/e/1FAIpQLSeoBzHbqzAhVgkKyozSdhYTrME2UftqDGqMLJCLBolbxalz4g/viewform",
         permanent: false,
         basePath: false,
       },
