5e2c22c022db2d732bbed3ac9352f787daca99c3
So Nishimura <s.nishimura@gemcook.com>
2026-01-17 15:42:09 +0900

    feature: update nextConfig output

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
+  output: "standalone",
   images: {
     unoptimized: true,
   },
