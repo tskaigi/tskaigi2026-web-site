ad294c2c01e8f9e0df5bd021f68dedbee6f2d4ab
So Nishimura <s.nishimura@gemcook.com>
2026-01-17 15:24:25 +0900

    feature: cloudflare wrangler init

--- next.config.ts
+++ next.config.ts
@@ -11,4 +11,9 @@ const nextConfig: NextConfig = {
     process.env.IS_DEVELOPMENT === "true" ? ["tsx", "dev.tsx"] : ["tsx"],
 };
 
+// Enable calling `getCloudflareContext()` in `next dev`.
+// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+initOpenNextCloudflareForDev();
 export default nextConfig;
