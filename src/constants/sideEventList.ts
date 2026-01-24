9e8deb5e22f1b9d17e47ed91ccee560624200b3f
masayaO <27627295+masayaO@users.noreply.github.com>
2026-01-24 13:30:31 +0000

    feat: add side event - TSKaigi Mashup #3 Type Challenges ハンズオン

--- src/constants/sideEventList.ts
+++ src/constants/sideEventList.ts
@@ -22,4 +22,17 @@ export const sideEventList: SideEvent[] = [
     sponsors: ["TSKaigi Association"],
     finishedAt: new Date("2026-02-09T21:30:00+09:00"),
   },
+  {
+    date: "1/27 (火)",
+    name: "TSKaigi Mashup #3 Type Challenges ハンズオン",
+    link: "https://typescript-jpc.connpass.com/event/377810/",
+    thumbnail:
+      "https://media.connpass.com/thumbs/98/4d/984d98d0c7cbe8b6f1cec1a3bca4411e.png",
+    detail: `TypeScriptの型にまつわるプログラミングクイズが収録された type-challenges の問題を解くハンズオンを行います。入門者向けの難易度を予定しています。
+
+当日に問題をいくつかピックアップし、実際に解いてみる → 解説 という形で行います。TypeScriptの経験が豊富なスタッフが講師として入り、適宜サポートします。 まだTypeScriptにあまり慣れていない方や、TypeChallengesを初めて知ったという方でも気軽にご参加ください！`,
+    tags: ["ハンズオン"],
+    sponsors: ["TSKaigi"],
+    finishedAt: new Date("2026-01-28T00:00:00+09:00"),
+  },
 ];
