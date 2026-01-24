639dea62cf34e401c8cc3cd4d55413ab5cd881cb
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2026-01-24 20:29:02 +0900

    feat: change top page wording for additional round

--- src/app/_components/SeekingSponsorsSection.tsx
+++ src/app/_components/SeekingSponsorsSection.tsx
@@ -7,12 +7,13 @@ export function SeekingSponsorsSection() {
     <div className="w-full flex flex-col items-center gap-16 px-8 pt-10 pb-16">
       <div className="w-full lg:w-[760px] flex flex-col items-center gap-10">
         <h2 className="text-[32px] lg:text-4xl leading-[48px] tracking-[-1.2%] font-bold text-center">
-          スポンサー募集開始
+          スポンサー2次募集
         </h2>
         <div className="px-0 md:px-10 font-normal text-m leading-[25px] tracking-[-0.75%]">
           <p>
             TypeScriptコミュニティの発展に共に取り組んでいただけるスポンサー企業を募集いたします。
             希望される企業様はスポンサー向け資料をご確認の上、お申し込みください。
+            現在は2次募集中です。
           </p>
         </div>
         <Button
