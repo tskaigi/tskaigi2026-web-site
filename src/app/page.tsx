46008807ee71dd78e901e3ecb09f088794cceaa2
Gityosan <ymo6190@docomo.ne.jp>
2026-02-01 00:19:33 +0900

    feat: update top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { EventCountdownBanner } from "./_components/EventCountdownBanner";
+import Image from "next/image";
+import { ApplyToProposalSection } from "@/components/ApplyToProposalSection";
+import { HeroSection } from "@/components/HeroSection";
+import { MissionSection } from "@/components/MissionSection";
 import { SeekingSponsorsSection } from "./_components/SeekingSponsorsSection";
 
 export default function Home() {
   return (
-    <main className="w-full flex flex-col flex-1 items-center font-outfit">
-      <EventCountdownBanner />
+    <main className="w-full flex flex-col flex-1 items-center font-outfit pt-[60px] md:pt-[68px]">
+      <HeroSection />
+      <MissionSection />
+      <ApplyToProposalSection />
       <SeekingSponsorsSection />
     </main>
   );
