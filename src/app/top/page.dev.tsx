f736c743a916fe4adfe4a1144e026fbfc4e80092
Gityosan <ymo6190@docomo.ne.jp>
2026-02-01 10:12:05 +0900

    feat: update pages

--- src/app/top/page.dev.tsx
+++ src/app/top/page.dev.tsx
@@ -1,13 +1,16 @@
-import { SponsorsBoardSection } from "@/components/SponsorsBoardSection";
-import { HeroSectionWithMotion } from "../../components/HeroSectionWithMotion";
-import { MissionSection } from "../../components/MissionSection";
+import Image from "next/image";
+import { ApplyToProposalSection } from "@/components/ApplyToProposalSection";
+import { HeroSection } from "@/components/HeroSection";
+import { MissionSection } from "@/components/MissionSection";
+import { SeekingSponsorsSection } from "./../_components/SeekingSponsorsSection";
 
 export default function Home() {
   return (
-    <main className="flex-1 pt-8 overflow-x-hidden">
-      <HeroSectionWithMotion />
+    <main className="w-full flex flex-col flex-1 items-center font-outfit pt-[60px] md:pt-[68px]">
+      <HeroSection />
       <MissionSection />
-      <SponsorsBoardSection />
+      <ApplyToProposalSection />
+      <SeekingSponsorsSection />
     </main>
   );
 }
