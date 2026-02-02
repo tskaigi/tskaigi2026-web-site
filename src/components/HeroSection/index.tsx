78910d3a4900c9ec74537d5d942cb28f088139e7
Gityosan <ymo6190@docomo.ne.jp>
2026-02-02 10:05:07 +0900

    feat: add key visual

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -3,12 +3,15 @@ import Image from "next/image";
 
 export function HeroSection() {
   return (
-    <Image
-      src="/key-visual-2026.png"
-      alt="TSKaigi"
-      width={1344}
-      height={484}
-      className="w-full"
-    />
+    <div className="w-full">
+      <Image
+        src="/key-visual-2026.svg"
+        alt="TSKaigi"
+        width={1344}
+        height={484}
+        className="w-full"
+      />
+      <div className="w-full h-10 bg-gradient-to-b from-white to-blue-light-100" />
+    </div>
   );
 }
