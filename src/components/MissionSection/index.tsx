623314f5fb6caa49cd4e0c52f40a16b79bfb9ea4
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2026-02-11 22:36:12 +0900

    fix: quotation

--- src/components/MissionSection/index.tsx
+++ src/components/MissionSection/index.tsx
@@ -9,7 +9,7 @@ function MissionSessionContent() {
       <p className="text-blue-500 text-xl font-bold text-center mt-1 lg:mt-4 md:text-2xl lg:text-3xl">
         学び、繋がり、
         <span className="text-2xl md:text-4xl lg:text-[42px] font-noto">
-          ”型”
+          “型”
         </span>
         を破ろう
       </p>
