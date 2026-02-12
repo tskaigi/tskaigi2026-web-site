3d5b12113b39613ee917cc3950acb0752ce2fd09
So Nishimura <s.nishimura@gemcook.com>
2026-02-12 21:15:02 +0900

    feat: build settings

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
+
+await initOpenNextCloudflareForDev();
 
 const projectRoot = process.cwd();
 
