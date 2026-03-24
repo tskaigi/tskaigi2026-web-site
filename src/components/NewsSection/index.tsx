0afbd792d7577e1b3abfccd4a3880be18e3416dc
kamekyame <kamekyame@outlook.com>
2026-03-24 23:56:40 +0900

    feat: 基調講演情報を追加

---
+++ src/components/NewsSection/index.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export const NewsSection = () => {
+  return (
+    <section id="news" className="w-full bg-blue-light-100">
+      <div className="bg-white md:rounded-2xl mx-auto grid max-w-[940px] gap-2 p-4 sm:grid-cols-[max-content_1fr] sm:p-6">
+        <div>2026.04.01（仮）</div>
+        <div>
+          <Link
+            href="#keynote"
+            className="text-link-light underline underline-offset-3 hover:no-underline"
+          >
+            基調講演
+          </Link>
+          を公開しました
+        </div>
+      </div>
+    </section>
+  );
+};
