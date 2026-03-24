7bf27e129761448adf0bb5f8afec50720827478f
kamekyame <kamekyame@outlook.com>
2026-03-25 08:57:32 +0900

    fix: 日付を修正

--- src/components/NewsSection/index.tsx
+++ src/components/NewsSection/index.tsx
@@ -4,7 +4,7 @@ export const NewsSection = () => {
   return (
     <section id="news" className="w-full bg-blue-light-100">
       <div className="bg-white md:rounded-2xl mx-auto grid max-w-[940px] gap-2 p-4 sm:grid-cols-[max-content_1fr] sm:p-6">
-        <div>2026.04.01（仮）</div>
+        <div>2026.03.26. </div>
         <div>
           <Link
             href="#keynote"
